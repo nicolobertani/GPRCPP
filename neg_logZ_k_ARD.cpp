761d84b152dc6f540313fab4d3cfba2bf8ed6ba0
Nicolò Bertani <nicolobertani@iMac-di-Casa.local>
2020-07-21 17:48:19 +0200

    Removed redundant definitions from neg_logZ

--- neg_logZ_k_ARD.cpp
+++ neg_logZ_k_ARD.cpp
@@ -10,12 +10,6 @@ double k_se(const vec &x, const vec &y, const double &m) {
   return res;
 }
 
-double d_k_se_m(const vec &x, const vec &y, const double &m) {
-  double df = pow(norm(x - y, 2), 2);
-  double res = df / pow(m, 3) * exp(- df / (2 * pow(m, 2)));
-  return res;
-}
-
 mat K_se(const mat &M, const mat &N, const double &m, const bool &equal_matrices) {
   // Rcout << "I am running.\n"; // progress message
   mat K;
@@ -45,38 +39,6 @@ mat K_se(const mat &M, const mat &N, const double &m, const bool &equal_matrices
     for (int r = 0; r < M.n_rows; r++) {
       for (int c = 0; c < N.n_rows; c++) {
         K(r, c) = k_se(M.row(r).t(), N.row(c).t(), m);
-      }
-    }
-  }
-  // Rcout << "Ok, I got to the end.\n"; // progress message
-  return K;
-}
-
-mat d_K_se(const mat &M, const mat &N, const double &m, const bool &equal_matrices) {
-  // Rcout << "I am running.\n"; // progress message
-  mat K;
-
-  if (equal_matrices == 1) {
-
-    // Rcout << "Matrices are equal.\n"; // progress message
-    K.set_size(M.n_rows, M.n_rows);
-    K.zeros();
-    // fill upper triangular wo diag
-    for (int r = 0; r < M.n_rows; r++) {
-      for (int c = r + 1; c < M.n_rows; c++) {
-        K(r, c) = d_k_se_m(M.row(r).t(), M.row(c).t(), m);
-      }
-    }
-    K = K + K.t();
-
-  } else {
-
-    // Rcout << "Matrices are NOT equal.\n"; // progress message
-    K.set_size(M.n_rows, N.n_rows);
-    // fill everything
-    for (int r = 0; r < M.n_rows; r++) {
-      for (int c = 0; c < N.n_rows; c++) {
-        K(r, c) = d_k_se_m(M.row(r).t(), N.row(c).t(), m);
       }
     }
   }
