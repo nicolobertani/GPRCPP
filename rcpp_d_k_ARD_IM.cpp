1ec7f340aa6e4a75e87e60903043070b7be6b5c6
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2020-09-04 16:14:16 +0200

    adeed d_K_IM_partial to d_k_ARD_IM

--- rcpp_d_k_ARD_IM.cpp
+++ rcpp_d_k_ARD_IM.cpp
@@ -4,6 +4,8 @@
 using namespace Rcpp;
 using namespace arma;
 
+double sq2pi = pow(2 * datum::pi, .5);
+
 double k_se(const vec &x, const vec &y, const double m) {
   double df = pow(norm(x - y, 2), 2);
   double res = exp(- df / (2 * pow(m, 2)));
@@ -17,12 +19,10 @@ double d_k_se_m(const vec &x, const vec &y, const double m) {
 }
 
 mat K_se(const mat &M, const mat &N, const double m, const bool equal_matrices) {
-  // Rcout << "I am running.\n"; // progress message
   mat K;
 
   if (equal_matrices == 1) {
 
-    // Rcout << "Matrices are equal.\n"; // progress message
     K.set_size(M.n_rows, M.n_rows);
     K.zeros();
     // fill upper triangular wo diag
@@ -39,7 +39,6 @@ mat K_se(const mat &M, const mat &N, const double m, const bool equal_matrices) 
 
   } else {
 
-    // Rcout << "Matrices are NOT equal.\n"; // progress message
     K.set_size(M.n_rows, N.n_rows);
     // fill everything
     for (int r = 0; r < M.n_rows; r++) {
@@ -48,17 +47,14 @@ mat K_se(const mat &M, const mat &N, const double m, const bool equal_matrices) 
       }
     }
   }
-  // Rcout << "Ok, I got to the end.\n"; // progress message
   return K;
 }
 
 mat d_K_se(const mat &M, const mat &N, const double m, const bool equal_matrices) {
-  // Rcout << "I am running.\n"; // progress message
   mat K;
 
   if (equal_matrices == 1) {
 
-    // Rcout << "Matrices are equal.\n"; // progress message
     K.set_size(M.n_rows, M.n_rows);
     K.zeros();
     // fill upper triangular wo diag
@@ -71,7 +67,6 @@ mat d_K_se(const mat &M, const mat &N, const double m, const bool equal_matrices
 
   } else {
 
-    // Rcout << "Matrices are NOT equal.\n"; // progress message
     K.set_size(M.n_rows, N.n_rows);
     // fill everything
     for (int r = 0; r < M.n_rows; r++) {
@@ -80,18 +75,75 @@ mat d_K_se(const mat &M, const mat &N, const double m, const bool equal_matrices
       }
     }
   }
-  // Rcout << "Ok, I got to the end.\n"; // progress message
   return K;
 }
 
+double d_k_IM_partial(const double &SUK_i, const double &dSUK_i, const double &SUK_j, const double &dSUK_j,
+                      const double &ell, const double &bandwidth, const int &nb) {
+  double res = pow((SUK_i - SUK_j) / (sq2pi * nb), 2) / (2 * pow(ell, 2) * bandwidth) -
+                (SUK_i - SUK_j) * (dSUK_i - dSUK_j) / pow(sq2pi * nb * ell, 2);
+  return res;
+}
+
+mat d_k_IM_partial(const vec &SUK_vec_x, const vec &dSUK_vec_x, const vec &SUK_vec_y, const vec &dSUK_vec_y,
+  const double &ell, const double &bandwidth, const int &n, const bool &equal_vec) {
+  mat K;
+  double nb = n * bandwidth;
+
+  if (equal_vec == 1) {
+
+    K.set_size(SUK_vec_x.n_elem, SUK_vec_x.n_elem);
+    K.zeros();
+    // fill upper triangular wo diag
+    for (int i = 0; i < SUK_vec_x.n_elem; i++) {
+      for (int j = i + 1; j < SUK_vec_x.n_elem; j++) {
+        K(i, j) = d_k_IM_partial(SUK_vec_x(i), dSUK_vec_x(i), SUK_vec_x(j), dSUK_vec_x(j), ell, bandwidth, nb);
+      }
+    }
+    K = K + K.t();
+    // fill diag
+    for (int i = 0; i < SUK_vec_x.n_elem; i++) {
+      K(i,i) = d_k_IM_partial(SUK_vec_x(i), dSUK_vec_x(i), SUK_vec_x(i), dSUK_vec_x(i), ell, bandwidth, nb);
+    }
+
+  } else {
+
+    K.set_size(SUK_vec_x.n_elem, SUK_vec_y.n_elem);
+    // fill everything
+    for (int i = 0; i < SUK_vec_x.n_elem; i++) {
+      for (int j = 0; j < SUK_vec_y.n_elem; j++) {
+        K(i, j) = d_k_IM_partial(SUK_vec_x(i), dSUK_vec_x(i), SUK_vec_y(j), dSUK_vec_y(j), ell, bandwidth, nb);
+      }
+    }
+  }
+  return K;
+}
+
+mat SUK_to_IM(const mat &SUK, const vec &b_vec, const vec &n_vec) {
+  mat IM;
+  IM.copy_size(SUK);
+  for (size_t i = 0; i < IM.n_rows; i++) {
+    for (size_t j = 0; j < IM.n_cols; j++) {
+      IM(i,j) = SUK(i,j) / (sq2pi * n_vec(j) * b_vec(j));
+    }
+  }
+  return(IM);
+}
+
+
 // [[Rcpp::export]]
-List rcpp_k_ARD(const mat &X, const mat &Y, const vec &p_vec, const bool equal_mx, const bool compute_ds) {
+List rcpp_k_ARD_IM(const mat &SUK_X, const mat &dSUK_X, const mat &SUK_Y, const mat &dSUK_Y,
+                   const vec &p_vec, const vec &b_vec, const vec &n_vec,
+                   const bool equal_mx, const bool compute_ds) {
+  // GENERATE X AND Y FROM SUK_X AND SUK_Y
+  // These are the Intensity Measures
+  mat X = SUK_to_IM(SUK_X, b_vec, n_vec);
+  mat Y = SUK_to_IM(SUK_Y, b_vec, n_vec);
   // initial checks
   if (!equal_mx) {
     if (X.n_cols != Y.n_cols) Rcout << "Unequal number of columns in the matrices.\n";
   }
   int p = p_vec.n_elem;
-  // Rcout << p << "\n";
   if (X.n_cols != (p - 1)) Rcout << "Unequal number of matrix columns and parameters.\n";
   // generate variables
   cube K_cube;
@@ -112,7 +164,6 @@ List rcpp_k_ARD(const mat &X, const mat &Y, const vec &p_vec, const bool equal_m
   // COMPUTE COVARIANCE MATRIX
   // populate list of covariance matrices
   for (size_t i = 0; i < (p - 1); i++) {
-    // Rcout << i << "\n";
     K_cube.slice(i) = K_se(X.col(i), Y.col(i), p_vec(i), equal_mx);
   }
   // multiply the matrices in the list
@@ -123,11 +174,9 @@ List rcpp_k_ARD(const mat &X, const mat &Y, const vec &p_vec, const bool equal_m
   // COMPUTE DERIVATIVE MATRICES
   // compute all individual derivative matrices, first for characteristic length-scales l
   if (compute_ds) {
-    // Rcout << "I got to the derivatives!\n";
     for (size_t i = 0; i < p - 1; i++) {
       d_cube.slice(i) = d_K_se(X.col(i), Y.col(i), p_vec(i), equal_mx);
     }
-    // Rcout << "I got computed d_cube fine!\n";
     for (size_t i = 0; i < p - 1; i++) {
       mat temp;
       temp.copy_size(K_0);
